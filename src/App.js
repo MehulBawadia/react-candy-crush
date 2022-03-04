be3460b459060ce3dc2d291f919dc0f7a4859a01
Mehul Bawadia <mehulbawadia@gmail.com>
2022-03-04 21:04:25 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import { useEffect, useState } from "react";
-
-const width = 8;
-const candyColors = ["blue", "yellow", "green", "purple", "red", "orange"];
+import Board from "./components/Board";
 
 const App = () => {
-  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
-
-  const createBoard = () => {
-    const randomColorArrangement = [];
-    for (let i = 0; i < width * width; i++) {
-      const randomColor =
-        candyColors[Math.floor(Math.random() * candyColors.length)];
-      randomColorArrangement.push(randomColor);
-    }
-    setCurrentColorArrangement(randomColorArrangement);
-  };
-
-  useEffect(() => {
-    createBoard();
-  }, []);
-
-  return (
-    <div className="app">
-      <div className="game">
-        {currentColorArrangement.map((color, index) => {
-          return (
-            <img key={index} style={{ backgroundColor: color }} alt={color} />
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <Board />;
 };
 
 export default App;
