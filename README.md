d9377357da68c6a59258c86f90556519447f43e9
Mehul Bawadia <mehulbawadia@gmail.com>
2022-03-07 19:38:27 +0530

    Added links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Candy Crush Game
 
 This is a Candy Crush Game using ReactJs.
+
+[Play the game here](https://MehulBawadia.github.io/react-candy-crush/)
--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ const App = () => {
     <>
       <h1 className="text-center">Candy Crush in ReactJs</h1>
       <Board />
+
+      <div className="links">
+        <a href="https://MehulBawadia/react-candy-crush" target="_blank">
+          Check the source code
+        </a>
+      </div>
     </>
   );
 };
